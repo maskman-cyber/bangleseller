8b9da490776c349d70589c54cde0a3eaae832ba1
maskman-cyber <abdulnusaif7@gmail.com>
2025-03-24 13:09:02 +0300

    Your commit message

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,13 @@ const { getFirestore } = require("firebase/firestore");
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCxSwgjU00C_R0WR_h32P1DxrlC4zCwLlo",
-    authDomain: "bangleseller.firebaseapp.com",
-    projectId: "bangleseller",
-    storageBucket: "bangleseller.firebasestorage.app",
-    messagingSenderId: "340563599767",
-    appId: "1:340563599767:web:5a28457f6f7c808b7864d2"
-  };
+  apiKey: "AIzaSyCxSwgjU00C_R0WR_h32P1DxrlC4zCwLlo",
+  authDomain: "bangleseller.firebaseapp.com",
+  projectId: "bangleseller",
+  storageBucket: "bangleseller.firebasestorage.app",
+  messagingSenderId: "340563599767",
+  appId: "1:340563599767:web:5a28457f6f7c808b7864d2"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
